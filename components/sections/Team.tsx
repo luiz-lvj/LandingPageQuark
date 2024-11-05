e1d78669c2733c0c1716f5350643bfb53a253074
Daniel Yuki <danielyukihiga1@gmail.com>
2024-11-05 15:57:31 -0300

    team imgs

--- components/sections/Team.tsx
+++ components/sections/Team.tsx
@@ -5,9 +5,9 @@ import { FaTwitter, FaTelegram, FaGithub } from 'react-icons/fa'
 import Image from 'next/image'
 
 const teamMembers = [
-  { name: 'Luiz Vasconcelos', profilePic: 'https://wallpapers.com/images/hd/olaf-the-lovable-snowman-from-disneys-frozen-greeting-you-with-a-smile-ys11kyjvdgypw70n.jpg', role: 'Co-Founder', twitter: 'https://x.com/lvj_luiz', telegram: 'https://t.me/luiz_lvj', github: 'https://github.com/luiz-lvj' },
-  { name: 'Daniel Yuki', profilePic: 'https://i.pinimg.com/474x/68/65/04/6865048101d52e285cd3e1f87e50293e.jpg', role: 'Co-Founder', twitter: 'https://x.com/DanielYukiHiga', telegram: 'https://t.me/DanielYukiHiga', github: 'https://github.com/DanielYuki' },
-  { name: 'Arthur Jacobina', profilePic: 'https://i.pinimg.com/1200x/14/1a/ab/141aab9e471868e7296655e89b962e42.jpg', role: 'Co-Founder', twitter: 'https://x.com/Arthur1Jacobina', telegram: 'https://t.me/arthurjacobina', github: 'https://github.com/Arthur-Jacobina' },
+  { name: 'Luiz Vasconcelos', profilePic: '/olaf.png', role: 'Co-Founder', twitter: 'https://x.com/lvj_luiz', telegram: 'https://t.me/luiz_lvj', github: 'https://github.com/luiz-lvj' },
+  { name: 'Daniel Yuki', profilePic: '/dano2.png', role: 'Co-Founder', twitter: 'https://x.com/DanielYukiHiga', telegram: 'https://t.me/DanielYukiHiga', github: 'https://github.com/DanielYuki' },
+  { name: 'Arthur Jacobina', profilePic: '/kowalski.png', role: 'Co-Founder', twitter: 'https://x.com/Arthur1Jacobina', telegram: 'https://t.me/arthurjacobina', github: 'https://github.com/Arthur-Jacobina' },
 ]
 
 const containerVariants = {
@@ -64,21 +64,21 @@ export default function Team() {
                 transition={{ type: "spring", stiffness: 200, damping: 50 }}
                 className="mb-6 mx-auto w-40 h-40 overflow-hidden rounded-xl"
               >
-                <img
-                  src={member.profilePic}
-                  alt={`${member.name}'s profile picture`}
-                  width={160}
-                  height={160}
-                  className="object-cover w-full h-full"
-                />
-                {/* WHEN RIGHT IMAGES, USE THE CODE BELOW */}
-                {/* <Image
+                {/* <img
                   src={member.profilePic}
                   alt={`${member.name}'s profile picture`}
                   width={160}
                   height={160}
                   className="object-cover w-full h-full"
                 /> */}
+                {/* WHEN RIGHT IMAGES, USE THE CODE BELOW */}
+                <Image
+                  src={member.profilePic}
+                  alt={`${member.name}'s profile picture`}
+                  width={160}
+                  height={160}
+                  className="object-cover w-full h-full"
+                />
               </motion.div>
               <h3 className="text-2xl font-semibold mb-2 text-primary">{member.name}</h3>
               <p className="text-muted-foreground mb-4">{member.role}</p>
